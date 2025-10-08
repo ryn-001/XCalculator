21b9aa0efafae57da5eb5a6cbc0a8425e6fb0ba7
Aryan Khaire <aryan.khaire001@gmail.com>
2025-10-08 21:54:16 +0530

    Minor changes

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,45 +1,50 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./Calculator.css"
 
-export default function Calculator(){
-    const [answer,setAnswer] = useState(" ");
-    const [input,setInput] = useState("")
+export default function Calculator() {
+    const [answer, setAnswer] = useState(" ");
+    const [input, setInput] = useState("")
 
     const handleClick = (e) => {
         const value = e.target.value;
 
-        if(value === "C"){
+        if (value === "C") {
             setAnswer(" ");
             setInput("");
-        }else if(value === "="){
-            try{
-                const result = eval(input);
-                setAnswer(result);
-            }catch(e){
+        } else if (value === "=") {
+            if (input.trim() === "") {
                 setInput("");
                 setAnswer("Error")
+            } else {
+                try {
+                    const result = eval(input);
+                    setAnswer(result);
+                } catch (e) {
+                    setInput("");
+                    setAnswer("Error")
+                }
             }
-        }else{
+        } else {
             setInput(input + value);
         }
     }
 
-    return(
-        <div className="calculator" style={{padding: "1rem"}}>
+    return (
+        <div className="calculator" style={{ padding: "1rem" }}>
             <h1>React Calculator</h1>
 
             <div>
-                <input type="text" className="calculator-input" readOnly value={input}/>
-                <div id="result" style={{padding: "1.5rem", color: "lightgray"}}>{answer}</div>
+                <input type="text" className="calculator-input" readOnly value={input} />
+                <div id="result" style={{ padding: "1.5rem", color: "lightgray" }}>{answer}</div>
 
                 <div className="buttons">
                     {["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "C", "0", "=", "/"].map(
-            (btn) => (
-              <button key={btn} value={btn} onClick={handleClick}>
-                {btn}
-              </button>
-            )
-          )}
+                        (btn) => (
+                            <button key={btn} value={btn} onClick={handleClick}>
+                                {btn}
+                            </button>
+                        )
+                    )}
                 </div>
             </div>
         </div>
