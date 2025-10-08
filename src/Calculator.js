8bd4be83a89495bc4d5c5a01331b14b6c6f5a726
Aryan Khaire <aryan.khaire001@gmail.com>
2025-10-08 21:43:10 +0530

    dne

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,10 +1,42 @@
 import {useState} from "react";
+import "./Calculator.css"
 
 export default function Calculator(){
+    const [answer,setAnswer] = useState(" ");
+    const [input,setInput] = useState("")
+
+    const handleClick = (e) => {
+        const value = e.target.value;
+
+        if(value === "C"){
+            setAnswer(" ");
+            setInput("");
+        }else if(value === "="){
+            const result = eval(input);
+            setAnswer(result);
+        }else{
+            setInput(input + value);
+        }
+    }
 
     return(
-        <div className="calculator">
-            
+        <div className="calculator" style={{padding: "1rem"}}>
+            <h1>React Calculator</h1>
+
+            <div>
+                <input type="text" className="calculator-input" readOnly value={input}/>
+                <div id="result" style={{padding: "1.5rem", color: "lightgray"}}>{answer}</div>
+
+                <div className="buttons">
+                    {["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "C", "0", "=", "/"].map(
+            (btn) => (
+              <button key={btn} value={btn} onClick={handleClick}>
+                {btn}
+              </button>
+            )
+          )}
+                </div>
+            </div>
         </div>
     )
 }
